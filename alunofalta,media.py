82b42fa52fe4267c3f7314d81f7618ec7d61863c
laurapadoan40 <laurapadoan40@gmail.com>
2024-04-30 16:34:47 -0300

    alunofalta,media.py

---
+++ alunofalta,media.py
@@ -0,0 +1,12 @@
+# entrada de dados
+aluno = input("Digite o nome do aluno: ")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("digite a terceira nota: "))
+falta = int(input("digite a quantidade de falta: "))
+media = (nota1+nota2+nota3)/3
+print ("Aluno: " + aluno)
+print ("Media: " + str(media))
+print ("falta: " + str(falta))
+
+
