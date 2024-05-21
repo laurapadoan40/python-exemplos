d6261a806851ee85e85155aefa31d13be8c93847
laurapadoan40 <laurapadoan40@gmail.com>
2024-05-21 14:56:46 -0300

    listas.py

---
+++ listas.py
@@ -0,0 +1,8 @@
+aluno = ["Laura Costa Padoan", 4, 8.5, False]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Média: {aluno[2]}")
+if aluno[3] == False: 
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado: Não")
